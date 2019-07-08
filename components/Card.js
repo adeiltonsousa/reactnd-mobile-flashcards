ca55091d93d1f798d9c887032142cba7af38ca9c
adeiltonsousa <adeiltonsousaxavier@gmail.com>
2019-07-08 17:47:57 -0300

    Styles para o Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,14 +1,19 @@
 import React, { PureComponent } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { NavigationActions } from 'react-navigation';
 import { addCardToDeck } from '../utils/api';
 import { red } from '../utils/colors';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import BtDefault from './BtDefault';
+import { addNewCard } from '../actions';
+import Button from './Button';
 
 class Card extends PureComponent {
   state = { 
     question: 'Question',
     answer: 'Answer',
+    questionTooShort: false,
+    answerTooShort: false
   };
 
   createCard = () => {
@@ -16,6 +21,21 @@ class Card extends PureComponent {
       const cardObj = {
         question: this.state.question,
         answer: this.state.answer
+      }
+      const deckTitle = this.props.navigation.state.params.deck;
+      addCardToDeck(deckTitle, cardObj);
+      this.props.addNewCard(deckTitle, cardObj);
+      this.setState({ 
+        question: '',
+        answer: ''
+      });
+      this.props.navigation.navigate('IndividualDeck', { deck: deckTitle });
+    } else {
+      if(this.state.question.length <= 6) {
+        this.setState({ questionTooShort: true })
+      }
+      if(this.state.answer.length <= 1) {
+        this.setState({ answerTooShort: true })
       }
     }
   }
@@ -39,12 +59,38 @@ class Card extends PureComponent {
           value={this.state.answer}
           onFocus={() => this.setState({ answer: '', answerTooShort: false })}
         />
-        <View style={styles.BtDefaultWrapper}>
-          <BtDefault text='Create Card' func={this.createCard}/>
+        <View style={styles.buttonWrapper}>
+          <Button text='Create Card' func={this.createCard}/>
         </View>
       </View>
     );
   }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'flex-start',
+  },
+  error: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: red
+  },
+  input: {
+    padding: 10,
+    marginTop: 15,
+    marginBottom: 10,
+    fontSize: 17
+  },
+  buttonWrapper: {
+    alignItems: "center"
+  }
+});
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ addNewCard }, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(Card);
